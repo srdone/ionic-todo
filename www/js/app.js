92a590c2d276fc5b4bfb07abe601b688a760f1c7
Stephen Done <srdone@gmail.com>
2015-04-14 21:33:43 -0600

    basic controller - time to actually build something

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('todo', ['ionic'])
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,4 +16,13 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.controller('TodoCtrl', function ($scope) {
+  $scope.tasks = [
+    {title: 'One little task went to market'},
+    {title: 'One little task stayed home'},
+    {title: 'One little task ate roast beef'},
+    {title: 'Made you look!'}
+  ];
+});
